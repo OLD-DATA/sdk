211fefc73860c10b4da037d8dd046e076a280830
Lucas <godkiller_nt@hammermaps.de>
2023-04-24 23:30:35 +0200

    Refactor Gauss gun stop event code
    ValveSoftware/halflife#3188

--- dlls/gauss.cpp
+++ dlls/gauss.cpp
@@ -120,7 +120,7 @@ BOOL CGauss::Deploy( )
 
 void CGauss::Holster( int skiplocal /* = 0 */ )
 {
-	PLAYBACK_EVENT_FULL( FEV_RELIABLE | FEV_GLOBAL, m_pPlayer->edict(), m_usGaussFire, 0.01, (float *)&m_pPlayer->pev->origin, (float *)&m_pPlayer->pev->angles, 0.0, 0.0, 0, 0, 0, 1 );
+	SendStopEvent(true);
 	
 	m_pPlayer->m_flNextAttack = UTIL_WeaponTimeBase() + 0.5;
 	
